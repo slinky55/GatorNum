52343fb3251eeab1d8a314278c635f968cae3572
gabo0802 <gabriel.castejon@ufl.edu>
2023-11-17 12:37:51 -0500

    changes to front-end

--- gatornum/src/App.js
+++ gatornum/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { useState } from 'react';
 
@@ -24,20 +23,28 @@ const OCRApp = () => {
       </header>
 
       {/* Main Content */}
-      <main className="flex-1 p-4">
+      <main
+        className="flex-1 p-4 bg-cover bg-opacity bg-gradient-to-t from-gray-700 to-gray-900"
+        style={{
+          /* Credit: https://www.pexels.com/photo/blue-and-purple-cosmic-sky-956999/  */
+          // backgroundImage: `url('/images/bg_image.jpg')`, // Add your image path here
+        }}
+      >
         <div className="mb-4">
-          <h2 className="text-xl font-bold mb-2">Description</h2>
-          <p className="text-gray-600">Your description goes here...</p>
+          <h2 className="text-xl text-center font-bold mb-2 text-white">Description</h2>
+          <p className="text-white text-center">Your description goes here...</p>
         </div>
 
         {/* Image Upload */}
-        <div className="mb-4">
+        <div className="flex flex-col items-center justify-center">
+        <div className="bg-white p-4 rounded-md">
           <input
             type="file"
             accept="image/*"
             onChange={handleImageUpload}
             className="border border-gray-300 p-2 rounded-md"
           />
+        </div>
         </div>
 
         {/* Image Placeholder */}
@@ -53,19 +60,20 @@ const OCRApp = () => {
 
         {/* Transcribed Text Box */}
         <div>
-          <h2 className="text-xl font-bold mb-2">Transcribed Text</h2>
+          <h2 className="text-xl font-bold mb-2 text-center text-white p-2">Transcribed Text</h2>
           <textarea
             value={transcribedText}
             onChange={(e) => setTranscribedText(e.target.value)}
             className="border border-gray-300 p-2 rounded-md w-full h-32 resize-none"
             placeholder="Transcribed text will appear here..."
+            readOnly
           ></textarea>
         </div>
       </main>
 
       {/* Footer */}
       <footer className="bg-gray-800 text-white p-4">
-        <p>&copy; 2023 Your Company</p>
+        <p>&copy; 2023 Gabriel Castejon & Larry Mason</p>
       </footer>
     </div>
   );
